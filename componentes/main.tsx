329a0f18876252253868661f217a3e9e673f308f
MatiasScythe <mjancidakis2020@alu.uct.cl>
2023-09-29 02:14:39 -0300

    Implementación Código QR
    -Se instalan los paquetes para el scanner QR y la camara
    - Se configura AndroidManifest, para los permisos como graddle para la camara
    - se crea eas.json, para el build de nuestra aplicacion cuando se requiera
    - Se modifica el main, para implementar la herramienta Scanner QR
    -Se guardan los datos del scanneo en qrData con exito

--- componentes/main.tsx
+++ componentes/main.tsx
@@ -1,48 +1,68 @@
-import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  Button,
-  ImageBackground,
-  StyleSheet,
-} from 'react-native';
-import App from '../App';
+import React, { useState, useRef } from 'react';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
+import { StyleSheet, View, Alert, TouchableOpacity, Text } from 'react-native';
 
 
 export const Main = () => {
+  const [qrData, setQrData] = useState<string | null>(null);
+  const qrScannerRef = useRef<QRCodeScanner | null>(null);
 
-  const [mostrarLogin, setMostrarLogin] = useState(false); 
-
-  const handleRegistro = () => {
-
-    if (true) {
-      setMostrarLogin(true);
-    }
+  const onSuccess = (e: any) => {
+    // Manejar el código QR leído aquí
+    const data = e.data;
+    setQrData(data); // se capta el mensaje del codigo qr
+    Alert.alert('Código QR leído', data);
   };
 
-  if (mostrarLogin) {
-    return <App/>;
-  }
   return (
-    
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-      <Button title="Volver al principio" onPress={handleRegistro} />
+    <View style={styles.ContenedorMain}>
+      {qrData && (
+        <View style={styles.ContenedorQR}>
+          <Text style={styles.TextoQR}>Código QR leído:</Text>
+          <Text style={styles.TextoQR}>{qrData}</Text>
+        </View>
+      )}
+
+      {/* Lector de códigos QR */}
+      <QRCodeScanner
+        ref={(node) => {
+          qrScannerRef.current = node;
+        }}
+        onRead={onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}  //Se habilita el flash, para captar mejor el codigo qr
+        topContent={
+          <View>
+            <Text>Scanner codigo QR</Text>
+          </View>
+        }
+      />
     </View>
-   
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  ContenedorMain: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    fontSize: 36,
-    color: 'red',
+  Camara: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: 'blue',
+    borderRadius: 30,
+    padding: 10,
+  },
+  ContenedorQR: {
+    marginTop: 20,
+    backgroundColor: '#eee',
+    padding: 10,
+    borderRadius: 5,
+  },
+  TextoQR: {
+    fontSize: 16,
     fontWeight: 'bold',
   },
 });
