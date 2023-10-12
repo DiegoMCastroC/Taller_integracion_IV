8ff6b78adc75a4918661a6cb527557cd1af11ffb
Dcastro <holasoydiego.dc@gmail.com>
2023-10-12 20:20:19 -0300

    Tienda ropa test
    es un modelo de como podemos mostrar los productos y tener un carro de compra, aun faltaria agregar la info del usuario

--- App.tsx
+++ App.tsx
@@ -1,108 +1,44 @@
-import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  Button,
-  ImageBackground,
-  StyleSheet,
-} from 'react-native';
-import fondo from './assets/4.png';
-import LoginScreen from './componentes/InicioSesion';
-import RegisterScreen from './componentes/Register';
-
-const App = () => {
-  const [currentScreen, setCurrentScreen] = useState('home');
-
-  const handleLoginButtonClick = () => {
-    setCurrentScreen('login');
-  };
-
-  const handleMainButtonClick = () => {
-    setCurrentScreen('RegisterScreen');
-  };
-
-  const renderScreen = () => {
-    switch (currentScreen) {
-      case 'home':
-        return (
-          <View style={styles.container}>
-            <ImageBackground source={fondo} style={styles.background}>
-              <Text style={[styles.title1, { marginTop: 120 }]}>Bienvenido a Bananashop</Text>
-              <View style={styles.button}>
-              <Button title="Iniciar Sesión" onPress={handleLoginButtonClick}  />
-              </View>
-              <View style={styles.button2}>
-              <Button title="Registrarse" onPress={handleMainButtonClick} />
-              </View>
-              
-            </ImageBackground>
-          </View>
-        );
-      case 'login':
-        return <LoginScreen />;
-      case 'RegisterScreen':
-        return (   
-              <RegisterScreen />
-        );
-      default:
-        return null;
-    }
-  };
-
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ProductsList } from './screens/ProductsList';
+import { ProductDetails } from './screens/ProductsDetails';
+import { Cart } from './screens/Cart';
+import { CartIcon } from './components/CartIcon';
+import { CartProvider } from './components/CartContext';
+const Stack = createNativeStackNavigator();
+function App() {
   return (
-    <View style={styles.container}>
-      {renderScreen()}
-    </View>
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Products' component={ProductsList} 
+          options={({ navigation }) => ({
+            title: 'Products',
+            headerTitleStyle: styles.headerTitle,
+            headerRight: () => <CartIcon navigation={navigation}/>
+          })}/>
+          <Stack.Screen name='ProductDetails' component={ProductDetails} 
+          options={({ navigation }) => ({
+            title: 'Product details',
+            headerTitleStyle: styles.headerTitle,
+            headerRight: () => <CartIcon navigation={navigation}/>,
+          })} />
+          <Stack.Screen name='Cart' component={Cart} 
+          options={({ navigation }) => ({
+            title: 'My cart',
+            headerTitleStyle: styles.headerTitle,
+            headerRight: () => <CartIcon navigation={navigation}/>,
+          })} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
   );
-};
-
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  background: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-  },
-  title1: {
-    fontSize: 40,
-    marginBottom: 16,
-    color: 'white',
-    textAlign: 'center',
-
-  },
-  title2: {
-    fontSize: 24,
-    marginBottom: 16,
-    color: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-  },
-  title3: {
-    fontSize: 15,
-    marginBottom: 16,
-    color: 'white',
-  },
-  
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 8,
-    marginBottom: 12,
-    backgroundColor: 'white',
-    textAlign: 'center',
-  },
-  button: {
-    marginBottom: 30, // Espacio adicional entre los botones
-  },
-  button2: {
-    marginBottom: 40, // Espacio adicional entre los botones
+  headerTitle: {
+    fontSize: 20
   }
 });
-
 export default App;
--- android/app/src/main/java/com/ta_4_test/MainActivity.java
+++ android/app/src/main/java/com/ta_4_test/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnative;
+package com.ta_4_test;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ReactNative";
+    return "ta_4_test";
   }
 
   /**
